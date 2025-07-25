87988914ff999bffb6e508c95b2fefb5b7771f02
vadim_dobrunin <vadim_dobrunin@mail.ru>
2025-07-25 16:43:06 +0300

    Добавлена коллекция Двусвязный список

---
+++ myCollections/LinkedList.go
@@ -0,0 +1,141 @@
+package myCollections
+
+import (
+	"errors"
+	"fmt"
+)
+
+type LinkedList struct {
+	Head *NodeLinkedList
+}
+
+type NodeLinkedList struct {
+	Val  int64
+	Prev *NodeLinkedList
+	Next *NodeLinkedList
+}
+
+// NewSinglyLinkedList возвращает объект "односвязный список"
+func NewLinkedList(val int64) *LinkedList {
+	return &LinkedList{Head: &NodeLinkedList{
+		Val:  val,
+		Prev: nil,
+		Next: nil,
+	}}
+}
+
+// Prepend добавляет элемент в начало списка
+func (list *LinkedList) Prepend(val int64) {
+	list.Head = &NodeLinkedList{
+		Val:  val,
+		Next: list.Head,
+	}
+}
+
+// Print печает в консоль весь двусвязный список(в прямом направлении)
+func (list *LinkedList) Print() {
+	v := *list.Head
+	for {
+		fmt.Println(v.Val)
+		if v.Next != nil {
+			v = *v.Next
+		} else {
+			break
+		}
+	}
+}
+
+// Append добавляет элемент в конец двусвязного списка
+func (list *LinkedList) Append(val int64) {
+	v := list.Head
+	for {
+		if v.Next != nil {
+			v = v.Next
+		} else {
+			break
+		}
+	}
+	v.Next = &NodeLinkedList{
+		Val:  val,
+		Prev: v,
+		Next: nil,
+	}
+}
+
+// RemoveByValue удаляет элемент по значению
+func (list *LinkedList) RemoveByValue(val int64) {
+	if list.Head.Val == val {
+		list.Head = list.Head.Next
+		return
+	}
+	v := list.Head
+	for {
+		if v.Next != nil {
+			if v.Next.Val == val {
+				v.Next = v.Next.Next
+				break
+			}
+			v = v.Next
+		} else {
+			break
+		}
+	}
+}
+
+// RemoveByIndex удаляет элемент по индексу
+func (list *LinkedList) RemoveByIndex(index uint64) {
+	if index == 0 {
+		list.Head = list.Head.Next
+		return
+	}
+	var ind uint64 = 1
+	v := list.Head
+	for true {
+		if v.Next != nil {
+			if ind == index {
+				v.Next = v.Next.Next
+				break
+			}
+			v = v.Next
+			ind++
+		} else {
+			break
+		}
+	}
+}
+
+// Find принимает на вход значение элемента списка и возвращает его индекс,
+// если элемент не найден, возвращает -1
+func (list *LinkedList) Find(val int64) int64 {
+	v := list.Head
+	for ind := int64(0); v != nil; ind++ {
+		if v.Val == val {
+			return ind
+		}
+		v = v.Next
+	}
+	return -1
+}
+
+// Size возвращает количество элементов в односвязном списке
+func (list *LinkedList) Size() uint64 {
+	var count uint64
+	v := list.Head
+	for v != nil {
+		count++
+		v = v.Next
+	}
+	return count
+}
+
+// GetValue возвращает элемент по индексу в двусвязном
+func (list *LinkedList) GetValue(index uint64) (int64, error) {
+	v := list.Head
+	for count := uint64(0); &v != nil; count++ {
+		if count == index {
+			return v.Val, nil
+		}
+		v = v.Next
+	}
+	return 0, errors.New(fmt.Sprintf("Not found element with index %d", index))
+}
---
+++ myCollections/linkedList_test.go
@@ -0,0 +1,131 @@
+package myCollections
+
+import (
+	//"myCollections"
+	"testing"
+)
+
+func TestGetValueLinkedList(t *testing.T) {
+	ml := NewLinkedList(777)
+	var want int64 = 777
+	got, _ := ml.GetValue(0)
+	if want != got {
+		t.Errorf("ml.GetValue(0) = %d; want %d", got, want)
+	}
+}
+
+func TestPrependLinkedList(t *testing.T) {
+	ml := NewLinkedList(777)
+	ml.Prepend(4)
+	ml.Prepend(3)
+	ml.Prepend(2)
+	ml.Prepend(1)
+	ml.Prepend(0)
+	var want [5]int64 = [5]int64{0, 1, 2, 3, 4}
+	var got [5]int64
+	got[0], _ = ml.GetValue(0)
+	got[1], _ = ml.GetValue(1)
+	got[2], _ = ml.GetValue(2)
+	got[3], _ = ml.GetValue(3)
+	got[4], _ = ml.GetValue(4)
+	if want != got {
+		t.Errorf("ml.GetValue(0) = %d; want %d", got[0], want[0])
+		t.Errorf("ml.GetValue(1) = %d; want %d", got[1], want[1])
+		t.Errorf("ml.GetValue(2) = %d; want %d", got[2], want[2])
+		t.Errorf("ml.GetValue(3) = %d; want %d", got[3], want[3])
+		t.Errorf("ml.GetValue(4) = %d; want %d", got[4], want[4])
+	}
+}
+
+func TestAppendLinkedList(t *testing.T) {
+	ml := NewLinkedList(777)
+	ml.Append(1)
+	ml.Append(2)
+	ml.Append(3)
+	ml.Append(4)
+	ml.Append(5)
+	var want [5]int64 = [5]int64{777, 1, 2, 3, 4}
+	var got [5]int64
+	got[0], _ = ml.GetValue(0)
+	got[1], _ = ml.GetValue(1)
+	got[2], _ = ml.GetValue(2)
+	got[3], _ = ml.GetValue(3)
+	got[4], _ = ml.GetValue(4)
+	if want != got {
+		t.Errorf("ml.GetValue(0) = %d; want %d", got[0], want[0])
+		t.Errorf("ml.GetValue(1) = %d; want %d", got[1], want[1])
+		t.Errorf("ml.GetValue(2) = %d; want %d", got[2], want[2])
+		t.Errorf("ml.GetValue(3) = %d; want %d", got[3], want[3])
+		t.Errorf("ml.GetValue(4) = %d; want %d", got[4], want[4])
+	}
+}
+
+func TestRemoveByValueLinkedList(t *testing.T) {
+	ml := NewLinkedList(777)
+	ml.Append(1)
+	ml.Append(2)
+	ml.Append(3)
+	ml.Append(4)
+	ml.Append(5)
+	var want [2]int64 = [2]int64{2, 3}
+	var got [2]int64
+	ml.RemoveByValue(777)
+	ml.RemoveByValue(1)
+	ml.RemoveByValue(4)
+	got[0], _ = ml.GetValue(0)
+	got[1], _ = ml.GetValue(1)
+	if want != got {
+		t.Errorf("ml.GetValue(0) = %d; want %d", got[0], want[0])
+		t.Errorf("ml.GetValue(1) = %d; want %d", got[1], want[1])
+	}
+}
+
+func TestRemoveByIndexLinkedList(t *testing.T) {
+	ml := NewLinkedList(777)
+	ml.Append(1)
+	ml.Append(2)
+	ml.Append(3)
+	ml.Append(4)
+	ml.Append(5)
+	var want [3]int64 = [3]int64{1, 3, 4}
+	var got [3]int64
+	ml.RemoveByIndex(0)
+	ml.RemoveByIndex(4)
+	ml.RemoveByIndex(1)
+	got[0], _ = ml.GetValue(0)
+	got[1], _ = ml.GetValue(1)
+	got[2], _ = ml.GetValue(2)
+	if want != got {
+		t.Errorf("ml.GetValue(0) = %d; want %d", got[0], want[0])
+		t.Errorf("ml.GetValue(1) = %d; want %d", got[1], want[1])
+		t.Errorf("ml.GetValue(2) = %d; want %d", got[2], want[2])
+	}
+}
+
+func TestSizeLinkedList(t *testing.T) {
+	ml := NewLinkedList(777)
+	ml.Append(1)
+	ml.Append(2)
+	ml.Append(3)
+	ml.Append(4)
+	ml.Append(5)
+	var want uint64 = 6
+	got := ml.Size()
+	if want != got {
+		t.Errorf("ml.Size = %d; want %d", got, want)
+	}
+}
+
+func TestFindLinkedList(t *testing.T) {
+	ml := NewLinkedList(777)
+	ml.Append(1)
+	ml.Append(2)
+	ml.Append(3)
+	ml.Append(4)
+	ml.Append(5)
+	var want int64 = 3
+	got := ml.Find(3)
+	if want != got {
+		t.Errorf("ml.Find(3) = %d; want %d", got, want)
+	}
+}
